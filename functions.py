21840e824191055b461aba7a73a1cbd0ffffc2de
crowdingcoder <smorgado25@gmail.com>
2026-06-09 12:49:43 -0400

    feat/main: comentarios menu

--- functions.py
+++ functions.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from main import *
 
 #1. Agregar libros
@@ -12,10 +11,3 @@ def agregar():
 #6. Eliminar libro
 #7. Modificar libro
 #8. Mostrar estadísticas
-=======
-
-
-def jaja():
-    libro = int(input("Ingrese un numero:"))
-    libros.append(libro)
->>>>>>> f0c0f99f74a25b566db203d57b78c82285a49c4b
