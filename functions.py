f799d063da45bcf10ad53df596d068962c2e95ca
crowdingcoder <smorgado25@gmail.com>
2026-06-09 12:34:38 -0400

    adios

---
+++ functions.py
@@ -0,0 +1,13 @@
+from main import *
+
+#1. Agregar libros
+def agregar():
+    print("Programiayua")
+
+#2. Mostrar libros
+#3. Buscar libro
+#4. Prestar libro
+#5. Devolver libro
+#6. Eliminar libro
+#7. Modificar libro
+#8. Mostrar estadísticas
