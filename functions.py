d29c6ec065d7b81c6a7ef4fceb1ea33fb2ff3d6b
nacho <AO-Alumno@duoc.acad>
2026-06-11 12:34:00 -0400

    ahora si xd

--- functions.py
+++ functions.py
@@ -26,8 +26,16 @@ def agregar_libros():
 #2. Mostrar libros
 
 def mostrar_libros():
-    print("opcion2")
+    print ("opcion2")
 
+    print("----Libros registrados----")
+    for l in libros:
+        print(f"----Libro---")
+        print(f"Titulo: ")
+        print()
+        print()
+        print()
+        
 #3. Buscar libro
 
 def buscar_libro():
@@ -57,3 +65,14 @@ def modificar_libro():
 
 def mostrar_estadisticas():
     print("opcion8")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
