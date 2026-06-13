c34d928754054398307964314d602b7b5d864c69
Ignacio Farías <ifarias1009@gmail.com>
2026-06-13 19:10:18 -0400

    Actualización funciones 1-6 / añadir .gitignore

--- main.py
+++ main.py
@@ -21,25 +21,38 @@ while True:
     opcion = int(input("Ingrese una opción:\n"))
 
     if opcion == 1:
+        os.system("cls")
         agregar_libros()
+        time.sleep(2)
     elif opcion == 2:
+        os.system("cls")
         mostrar_libros()
+        time.sleep(2)
     elif opcion == 3:
+        os.system("cls")
         buscar_libro()
+        time.sleep(2)
     elif opcion == 4:
+        os.system("cls")
         prestar_libro()
+        time.sleep(2)
     elif opcion == 5:
         os.system("cls")
         devolver_libro()
+        time.sleep(2)
         time.sleep(2)
     elif opcion == 6:
         os.system("cls")
         eliminar_libro()
         time.sleep(2)
     elif opcion == 7:
+        os.system("cls")
         modificar_libro()
+        time.sleep(2)
     elif opcion == 8:
+        os.system("cls")
         mostrar_estadisticas()
+        time.sleep(2)
     elif opcion == 9:
         banderita = False
     else:
