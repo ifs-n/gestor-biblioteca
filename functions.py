0d3865ddb927bc94ebe886f66283fd0b8c0b993f
crowdingcoder <crowdingcoder@192.168.1.24>
2026-06-13 17:46:13 -0400

    programiavanzado

--- functions.py
+++ functions.py
@@ -1,5 +1,18 @@
 libros = []
-flag1 = True
+
+
+#funciones extra
+def validacion_largo(validacion):
+    if len(validacion) >= 3:
+        return True
+    else:
+        return False
+    
+def disponibilidad(disponible):
+    if disponible == True:
+        print("Estado: Disponible.")
+    else:
+        print("Estado: No disponible.")
 
 
 #1. Agregar libros
@@ -7,36 +20,96 @@ flag1 = True
 def agregar_libros():
     
     print("----Ingreso libro----")
-    while flag1:
-        titulo = input("Ingrese titulo libro:\n")
+    while True:
+        titulo = input("Ingrese titulo libro:\n").capitalize()
+        validacion_largo(titulo)
+        while validacion_largo(titulo) == False:
+            print("El titulo ingresado es muy corto, debe tener al menos 3 caracteres.")
+            titulo = input("Ingrese titulo libro:\n").capitalize()
+            validacion_largo(titulo)
+
         if len(libros) > 0:
             for l in libros:
-                if l["titulo"] == titulo:
-                    print("El nombre ingresado se repite ")
-        else:
-            autor = input("Ingrese autor:")
-            estado = "Disponible"
-            libro = {
-                "nombre":titulo,
-                "autor":autor,
-                "estado":estado
-            }
-            flag1 = False
+                while l["titulo"] == titulo:
+                    print("El titulo ingresado se repite")
+                    titulo = input("Ingrese titulo libro:\n").capitalize()
+                    
+
+        
+        autor = input("Ingrese autor:\n").title()
+        validacion_largo(autor)
+        while validacion_largo(autor) == False:
+            print("El autor ingresado es muy corto, debe tener al menos 3 caracteres.")
+            autor = input("Ingrese autor libro:\n").title()
+            validacion_largo(autor)
+        estado = True
+        libro = {
+            "titulo":titulo,
+            "autor":autor,
+            "estado":estado
+        }
+        libros.append(libro)
+        break
 
 #2. Mostrar libros
 
 def mostrar_libros():
-    print("opcion2")
+    print("----Mostrar libros----")
+    if len(libros) <= 0:
+        print("\nNo se han registrado libros.\n")
+    else:
+        for l in libros:
+            print(f"Titulo:{l["titulo"]}")
+            print(f"Autor:{l["autor"]}")
+            disponibilidad(l["estado"])
+            print("------------")
 
 #3. Buscar libro
 
 def buscar_libro():
-    print("opcion3")
+    print("----Buscar libro----")
+    if len(libros) <= 0:
+        print("\nNo se han registrado libros.\n")
+    else:
+        buscar = input("Ingrese titulo libro a buscar:\n").capitalize()
+        encontrado = False
+        for l in libros:
+            if l["titulo"] == buscar:
+                encontrado = True
+                print(f"Titulo:{l["titulo"]}")
+                print(f"Autor:{l["autor"]}")
+                disponibilidad(l["estado"])
+        if encontrado == False:
+            print("El titulo ingresado no se encuentra registrado.")                 
 
 #4. Prestar libro
 
 def prestar_libro():
-    print("opcion4")
+    print("----Prestar libro----")
+    if len(libros) <= 0:
+        print("\nNo se han registrado libros.\n")
+    else:
+        prestar = input("Ingrese titulo libro:\n").capitalize()
+        prestado = False
+        for l in libros:
+            if l["titulo"] == prestar:
+                if l["estado"] == True:
+                    print("Se ha encontrado el libro y se encuentra disponible.")
+                    print(f"Titulo:{l["titulo"]}")
+                    print(f"Autor:{l["autor"]}")
+                    disponibilidad(l["estado"])
+                    l["estado"] = False
+                    prestado = True
+                elif l["estado"] == False:
+                    print("Se ha encontrado el libro pero no se encuentra disponible.")
+                    print(f"Titulo:{l["titulo"]}")
+                    print(f"Autor:{l["autor"]}")
+                    disponibilidad(l["estado"])
+                    prestado = True
+        if prestado == False:
+            print("El tirulo ingresado no se encuentra registrado.")
+
+
 
 #5. Devolver libro
 
